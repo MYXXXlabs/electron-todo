23f9e38b7c962e45a61b086eb6cd984763c4d66f
Myff <64042275+trymyff@users.noreply.github.com>
2020-06-15 18:47:38 +1200

    Finish appbar and start todo

--- src/scripts/js/titlebar.js
+++ src/scripts/js/titlebar.js
@@ -6,6 +6,11 @@ function minimise() {
     remote.BrowserWindow.getFocusedWindow().minimize();
 }
 function maximise() {
+    if(remote.BrowserWindow.getFocusedWindow().isMaximized()) {
+        maximised = true;
+    } else {
+        maximised = false;
+    }
     if(!maximised) {
         remote.BrowserWindow.getFocusedWindow().maximize();
         maximised = true;
@@ -14,6 +19,6 @@ function maximise() {
         maximised = false;
     }
 }
-function close() {
+document.getElementById("close").addEventListener("click", function() {
     remote.getCurrentWindow().close();
-}
+});
